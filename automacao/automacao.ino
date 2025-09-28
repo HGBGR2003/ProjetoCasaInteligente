309a7cc5922b9bf3bc5ebb7157a9280be03d7dce
MGuerra50 <matheusguerra50@gmail.com>
2025-09-28 17:58:06 -0300

    fix: atualização do código do arduíno para o projeto de automatização de iluminação

--- automacao/automacao.ino
+++ automacao/automacao.ino
@@ -1,39 +1,75 @@
 /*Alunos: Henrique Gabriel; Matheus Guerra;*/
 
-const int pinoRelePortao = 7;
-const int pinoSensorPortao = 2;
-const int pinoReleCasa = 5;
-const int pinoSensorCasa = 4;
-const long delayPadrao = 5000;
-const int pinoA1 = A1;
-int limiarEscuro = 500;
+const int pinoReleCasa = 5; 
+const int pinoSensorCasa = 4;    
+const int pinoA1 = A1;         
+unsigned long tempoEspera = 10000UL;
+int limiarLuminosidade = 400;
+bool ldrMaiorComLuz = true;
+bool releCasaLigado = false;
+unsigned long tempoUltimoMovimento = 0;
+const bool moduloReleActiveLow = true;
 
-void setup (){
-  pinMode(pinoRelePortao, OUTPUT);
-  pinMode(pinoSensorPortao, INPUT);
+void setup() {
   pinMode(pinoReleCasa, OUTPUT);
   pinMode(pinoSensorCasa, INPUT);
   pinMode(pinoA1, INPUT);
+
+  if (moduloReleActiveLow) digitalWrite(pinoReleCasa, HIGH);
+  else digitalWrite(pinoReleCasa, LOW);
+
+  Serial.begin(9600);
+  Serial.println("___________________________ Iniciando automacao de iluminacao ___________________________");
+  delay(500);
 }
 
-int sensorPortao = 0;
-int sensorCasa = 0;
-
-void loop (){
-
-  int valorDaLumisiodade = analogRead(pinoA1);
-
-  if(digitalRead(pinoSensorPortao) == HIGH && valorDaLumisiodade > limiarEscuro){
-    digitalWrite(pinoRelePortao, HIGH);
-    delay(delayPadrao);
-  } else {
-    digitalWrite(pinoRelePortao, LOW);
+void loop() {
+  int valorLDR = analogRead(pinoA1);
+  int leituraPIR = digitalRead(pinoSensorCasa);
+  if(leituraPIR == 0){
+    Serial.println("Tudo PARADO!");
   }
 
-  if(digitalRead(pinoSensorCasa) == HIGH && valorDaLumisiodade > limiarEscuro){
-    digitalWrite(pinoReleCasa, HIGH);
-    delay(delayPadrao);
-  } else {
-    digitalWrite(pinoReleCasa, LOW);
+  if(leituraPIR == 1){
+    Serial.println("TEM MOVIMENTO ai!");
   }
+
+  unsigned long agora = millis();
+
+  bool ambienteClaro;
+  if (ldrMaiorComLuz) ambienteClaro = (valorLDR >= limiarLuminosidade);
+  else ambienteClaro = (valorLDR <= limiarLuminosidade);
+
+  if (ambienteClaro) {
+    desligarRele();
+    tempoUltimoMovimento = 0;
+    delay(50);
+    return;
+  }
+
+  if (leituraPIR == HIGH) {
+    tempoUltimoMovimento = agora;
+    if (!releCasaLigado) {
+      ligarRele();
+    }
+  }
+
+  if (releCasaLigado && tempoUltimoMovimento != 0 && (agora - tempoUltimoMovimento) > tempoEspera) {
+    desligarRele();
+    tempoUltimoMovimento = 0;
+  }
+
+  delay(50);
+}
+
+void ligarRele() {
+  if (moduloReleActiveLow) digitalWrite(pinoReleCasa, LOW);
+  else digitalWrite(pinoReleCasa, HIGH);
+  releCasaLigado = true;
+}
+
+void desligarRele() {
+  if (moduloReleActiveLow) digitalWrite(pinoReleCasa, HIGH);
+  else digitalWrite(pinoReleCasa, LOW);
+  releCasaLigado = false;
 }
